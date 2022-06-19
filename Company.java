5fe861e6847f6b479629037d785380791b3ddd03
githubvetri <venkatachalam2233@gmail.com>
2022-06-19 20:23:51 +0530

    creategit

---
+++ Company.java
@@ -0,0 +1,16 @@
+package org.specifier;
+
+public class Company extends Employee  {
+	
+
+	public static void main(String[] args) {
+
+		//without extends
+		//System.out.println(Employee.age);
+		
+		
+		// with
+		System.out.println(age);
+
+	}
+}
---
+++ Employee.java
@@ -0,0 +1,19 @@
+package org.specifier;
+
+public class Employee {
+	static int age;
+	public void add() {
+		 int a =10;
+		System.out.println(a);
+	}
+		
+		
+		
+	
+	
+	
+	public static void main(String[] args) {
+	//Employee s  = new Employee();
+		//System.out.println(age);
+	}
+}
